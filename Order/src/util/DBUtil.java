971a3ceeb5e957a9b1d12409e2053f67bf61f245
achandrawar <76238878+achandrawar@users.noreply.github.com>
2023-12-27 17:22:12 +0530

    Update DBUtil.java

--- Order/src/util/DBUtil.java
+++ Order/src/util/DBUtil.java
@@ -1,7 +1,10 @@
 package util;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBUtil {
     static final String DB_URL = "jdbc:mysql://localhost:3306/order_management_system";
@@ -11,12 +14,29 @@ public class DBUtil {
 
     public static Connection getDBConn() {
         try {
-            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String filePath = "C:/Users/ayush/Desktop/JDBC/Order/src/db.properties";
+            FileInputStream fis = new FileInputStream(filePath);
+            Properties properties = new Properties(); 
+            properties.load(fis);
+            String url = properties.getProperty("url");
+	        String username = properties.getProperty("username");
+	        String password = properties.getProperty("password");
+
+	        connection = DriverManager.getConnection(url, username, password);
             return connection;
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | IOException | SQLException e) {
             e.printStackTrace();
         }
         return null;
+        
+        // try {
+        //     connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        //     return connection;
+        // } catch (SQLException e) {
+        //     e.printStackTrace();
+        // }
+        // return null;
     }
 
 }
